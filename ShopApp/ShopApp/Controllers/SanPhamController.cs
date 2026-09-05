bf28a747d18f587313a667f0bbbc1430f00654e9
tranduy2105 <tranducduy.ninhbinh@gmail.com>
2026-09-05 16:42:21 +0700

    style: them du lieu san pham

--- ShopApp/ShopApp/Controllers/SanPhamController.cs
+++ ShopApp/ShopApp/Controllers/SanPhamController.cs
@@ -15,6 +15,7 @@ namespace ShopApp.Controllers
             new SanPham { Id = 1, Ten = "Áo thun nam basic", GiaBan = 250000, TonKho = 15, DanhMuc = "Áo" },
             new SanPham { Id = 2, Ten = "Quần jean slim fit", GiaBan = 450000, TonKho = 10, DanhMuc = "Quần" },
             new SanPham { Id = 3, Ten = "Giày sneaker trắng", GiaBan = 800000, TonKho = 5,  DanhMuc = "Giày" },
+            new SanPham { Id = 4, Ten = "Mũ lưỡi trai", GiaBan = 150000, TonKho = 20, DanhMuc = "Phụ kiện" }
         };
 
         // GET: api/SanPham
